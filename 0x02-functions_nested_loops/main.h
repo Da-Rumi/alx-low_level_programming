5024ddacca5335704d39052f8cb39f5f6cbf89a8
Da-Rumi <kingosway@gmail.com>
2024-06-25 16:40:03 -0700

    3-islower.c

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,13 +1,8 @@
-<<<<<<< HEAD
 #ifndef MAIN_HEADER_FILE
 #define MAIN_HEADER_FILE
-=======
-#ifndef MAIN_H
-#define MAIN_H
 int _putchar(char);
 void print_alphabet(void);
 void print_alphabet_x10(void);
->>>>>>> 24c23c25959c794a5dafe652f4d746df0b8c9031
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
